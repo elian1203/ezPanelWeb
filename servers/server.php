b1c90e799b6e286b3501ad8ad11182b528eb9fd2
Elian <elian1203@gmail.com>
2021-08-23 18:56:33 -0400

    Remove pointless details files

--- servers/server.php
+++ servers/server.php
@@ -41,10 +41,110 @@ if (isset($GLOBALS['server']) && isset($_GET['detailsOnly']) && $_GET['detailsOn
               Server Not Found!
             </div>';
     return;
-  } else {
-    include('server_details.php');
   }
   ?>
 </div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-9">
+      <div class="container">
+        <h3><?php echo $GLOBALS['server']->id; ?>: <?php echo $GLOBALS['server']->name; ?></h3>
+        <h5>Status: <span id="status"><?php echo $GLOBALS['server']->status; ?></span></h5>
+        <p>
+          Online Players: <span id="onlinePlayers"><?php echo $GLOBALS['server']->onlinePlayers ?></span> <br>
+          Maximum Players: <span id="maxPlayers"><?php echo $GLOBALS['server']->maxPlayers ?></span>
+        </p>
+      </div>
+
+
+    </div>
+    <div class="col-md-3">
+      <div<?php
+      $perm = $GLOBALS['current_user']->permissions;
+      $serverId = $GLOBALS['server']->id;
+
+      error_log($perm);
+      error_log($perm != '*');
+      error_log($serverId);
+      error_log('/server.' . $serverId . '.[*]/');
+      error_log(!preg_match('/server.' . $serverId . '.[*]/', $perm));
+      error_log(!preg_match('/server.' . $serverId . '.commands/', $perm));
+
+      if ($perm != '*' && !preg_match('/server.' . $serverId . '.[*]/', $perm)
+        && !preg_match('/server.' . $serverId . '.commands/', $perm)) {
+        echo ' style="display: none;"';
+      }
+      ?>>
+        <button type="button" class="btn btn-primary" onclick="start()" id="button-start">Start</button>
+        <button type="button" class="btn btn-primary" onclick="stop()" id="button-stop">Stop</button>
+        <button type="button" class="btn btn-primary" onclick="restart()" id="button-restart">Restart</button>
+        <button type="button" class="btn btn-primary" onclick="kill()" id="button-kill">Kill</button>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-9">
+      <div class="card"<?php
+      $perm = $GLOBALS['current_user']->permissions;
+      $serverId = $GLOBALS['server']->id;
+
+      if ($perm != '*' && !preg_match('/server.' . $serverId . '.[*]/', $perm)
+        && !preg_match('/server.' . $serverId . '.console/', $perm)) {
+        echo ' style="display: none;"';
+      }
+      ?>>
+        <div class="card-header">
+          Console
+        </div>
+        <div class="card-body">
+          <ul id="console-logs" class="list-scrollable list-no-bullets">
+            <?php
+            $logs = $GLOBALS['server']->logs;
+            $count = count($logs);
+            for ($i = 0; $i < 500; $i++) {
+              if ($i < $count)
+                echo '<li class="console-log" id="log' . $i . '">' . $logs[$i] . '</li>';
+              else
+                echo '<li class="console-log" id="log' . $i . '" hidden="true"></li>';
+            }
+            ?>
+          </ul>
+          <input id="command" type="text" placeholder="Enter console command" style="width: 90%;">
+          <button class="btn btn-primary" type="button" onclick="sendCommand()">Send</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-3">
+      <div class="card"<?php
+      $userPerm = $GLOBALS['current_user']->permissions;
+      $perm = '/server.' . $GLOBALS['server']->id . '.ftp/';
+      $perm2 = '/server.' . $GLOBALS['server']->id . '.[*]]/';
+      if ($GLOBALS['ftpPort'] == -1 ||
+        (!preg_match($perm, $userPerm) && !preg_match($perm2, $userPerm) && $userPerm !== '*'))
+        echo ' style="display: none;"';
+      ?>>
+        <div class="card-header">FTP</div>
+        <div class="card-body">
+          <p class="card-text fw-bold">You can access the server's files using FileZilla with these connection
+            properties</p>
+          <p class="card-text">
+            Host: <?php echo file_get_contents('http://ipecho.net/plain/'); ?> <br>
+            Port: <?php echo $GLOBALS['ftpPort']; ?> <br>
+            User: <?php echo $GLOBALS['current_user']->username . '.' . $GLOBALS['server']->id ?> <br>
+            Pass: <span class="fst-italic">Your password</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+  document.getElementById('console-logs').addEventListener('scroll', onLogsScroll);
+  scrollLogsToBottom();
+
+  document.getElementById('command').addEventListener('keypress', commandKeyPressed);
+
+  updateDetails();
+</script>
 </body>
 </html>
--- settings/settings.php
+++ settings/settings.php
@@ -11,6 +11,56 @@ if (isset($_GET['id'])) {
   }
 }
 ?>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $requiredFields = ['name', 'javaPath', 'serverJar', 'jarPathRelativeTo', 'maximumMemory', 'autoStart', 'ftp', 'owner'];
+  $data = new stdClass();
+  $missing = false;
+  for ($i = 0; $i < count($requiredFields); $i++) {
+    $field = $requiredFields[$i];
+    if (!isset($_POST[$field]) || $_POST[$field] == '') {
+      $missing = true;
+      break;
+    } else {
+      $data->$field = $_POST[$field];
+    }
+  }
+
+  $serverJar = $data->serverJar;
+  if ($serverJar == 'custom') {
+    error_log('custom');
+    if (isset($_POST['custom-jar']) && $_POST['custom-jar'] != '') {
+      $serverJar = $_POST['custom-jar'];
+    } else {
+      $missing = true;
+    }
+  } else if (preg_match('/paper-[0-9a-zA-Z-.]+/', $serverJar)) {
+    $split = explode('-', $serverJar);
+    $url = 'https://papermc.io/api/v2/projects/' . $split[0] . '/versions/' . $split[1];
+    $response = json_decode(file_get_contents($url));
+
+    $latestBuild = $response->builds[count($response->builds) - 1];
+
+    $serverJar .= '-' . $latestBuild . '.jar';
+  } else if ($serverJar == 'waterfall-latest') {
+    $versions = json_decode(file_get_contents('https://papermc.io/api/v2/projects/waterfall'))->versions;
+    $latestVersion = $versions[count($versions) - 1];
+    $builds = json_decode(file_get_contents('https://papermc.io/api/v2/projects/waterfall/versions/' . $latestVersion))->builds;
+    $latestBuild = $builds[count($builds) - 1];
+
+    $serverJar = 'waterfall-' . $latestVersion . '-' . $latestBuild . '.jar';
+  }
+
+  $data->serverJar = $serverJar;
+
+  if ($missing == false) {
+    call('/servers/update/' . $GLOBALS['server']->id, json_encode($data));
+    header("Location:/settings");
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -37,11 +87,183 @@ if (isset($_GET['id'])) {
               Server Not Found!
             </div>';
     return;
-  } else {
-    include('settings_server.php');
   }
   ?>
 </div>
+<div class="container-fluid main-container">
+  <div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6 content col-md-offset-1">
+
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Edit Server - <?php echo $GLOBALS['server']->id ?></h5>
+          <div
+            class="alert alert-danger <?php
+            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+              echo 'no-display';
+            }
+            ?>"
+            role="alert">
+            Missing required options!
+          </div>
+          <form method="post">
+            <label for="name">
+              <h6 class="card-subtitle mb-2 text-muted">Server Name</h6>
+            </label>
+            <br/>
+            <input class="form-control" type="text" name="name" id="name"
+                   value="<?php echo $GLOBALS['server']->name ?>"/>
+            <br/>
+            <label for="javaPath">
+              <h6 class="card-subtitle mb-2 text-muted">Java Version</h6>
+            </label>
+            <br/>
+            <select class="form-select" name="javaPath" id="javaPath">
+              <option value="/bin/java">Server Default</option>
+              <?php
+              $response = json_decode(call('/server/javaVersions', ''));
+              foreach ($response as $versionName => $javaPath) {
+                $selected = $GLOBALS['server']->javaPath == $javaPath ? 'selected' : '';
+                echo '<option value="' . $javaPath . '" ' . $selected . '>' . $versionName . '</option>';
+              }
+              ?>
+            </select
+            <br/>
+            <label for="serverJar">
+              <h6 class="card-subtitle mb-2 text-muted">Server Jar</h6>
+            </label>
+            <br/>
+            <select class="form-select" name="serverJar" id="serverJar" onchange="jarChanged(this)"
+                    jar="<?php echo $GLOBALS['server']->serverJar ?>">
+              <optgroup label="Paper">
+                <?php
+                $response = json_decode(file_get_contents('https://papermc.io/api/v2/projects/paper'));
+                $versionGroups = $response->version_groups;
+
+                $versions = [];
+                for ($i = 0; $i < count($response->versions); $i++) {
+                  array_push($versions, $response->versions[$i]);
+                }
+
+                $jarExploded = explode('-', $GLOBALS['server']->serverJar);
+
+                for ($i = 0; $i < count($versionGroups); $i++) {
+                  $versionGroup = $versionGroups[$i];
+                  echo '<optgroup label="&nbsp;&nbsp;&nbsp;&nbsp;' . $versionGroup . '">';
+                  for ($j = 0; $j < count($versions); $j++) {
+                    $version = $versions[$j];
+                    if (preg_match('/' . $versionGroup . '/', $version)) {
+                      $versionSelected = ($jarExploded[0] == 'paper' && $jarExploded[1] == $version) ? 'selected' : '';
+                      echo '<option value="paper-' . $version . '" ' . $versionSelected . ' >Paper - '
+                        . $version . '</option>';
+                    }
+                  }
+                  echo '</optgroup>';
+                }
+                ?>
+              </optgroup>
+              <optgroup label="Waterfall (BungeeCord Alternative)">
+                <option value="waterfall-latest"<?php
+                if (preg_match('/waterfall-[0-9a-zA-Z.]+-[0-9]+/', $GLOBALS['server']->serverJar))
+                  echo 'selected';
+                ?>>Waterfall - Latest
+                </option>
+              </optgroup>
+              <optgroup label="Custom">
+                <option value="custom"<?php
+                if (!preg_match('/(paper|waterfall)-[0-9a-zA-Z.]+-[0-9]+/', $GLOBALS['server']->serverJar))
+                  echo 'selected';
+                ?>>Custom Jar
+                </option>
+              </optgroup>
+            </select>
+            <br/>
+            <label for="jarPathRelativeTo">
+              <h6 class="card-subtitle mb-2 text-muted">Server Jar Relative To</h6>
+            </label>
+            <br/>
+            <select class="form-select" name="jarPathRelativeTo" id="jarPathRelativeTo">
+              <option<?php if ($GLOBALS['server']->jarPathRelativeTo == 'Server Jar Folder') echo ' selected'; ?>>Server
+                Jar Folder
+              </option>
+              <option<?php if ($GLOBALS['server']->jarPathRelativeTo == 'Server Base Directory') echo ' selected'; ?>>
+                Server Base Directory
+              </option>
+              <option<?php if ($GLOBALS['server']->jarPathRelativeTo == 'Absolute') echo ' selected'; ?>>Absolute
+              </option>
+            </select>
+            <br/>
+            <label for="maximumMemory">
+              <h6 class="card-subtitle mb-2 text-muted">Maximum Memory</h6>
+            </label>
+            <br/>
+            <input class="form-control" type="text" name="maximumMemory" id="maximumMemory"
+                   value="<?php echo $GLOBALS['server']->maximumMemory; ?>"/>
+            <br/>
+            <label for="autoStart">
+              <h6 class="card-subtitle mb-2 text-muted">Auto Start</h6>
+            </label>
+            <br/>
+            <select class="form-select" name="autoStart" id="autoStart">
+              <option<?php if ($GLOBALS['server']->autoStart == true) echo ' selected'; ?>>True</option>
+              <option<?php if ($GLOBALS['server']->autoStart == false) echo ' selected'; ?>>False</option>
+            </select>
+            <br/>
+            <label for="ftp">
+              <h6 class="card-subtitle mb-2 text-muted">FTP Enabled</h6>
+            </label>
+            <select class="form-select" name="ftp" id="ftp">
+              <option<?php if ($GLOBALS['server']->ftp == true) echo ' selected'; ?>>True</option>
+              <option<?php if ($GLOBALS['server']->ftp == false) echo ' selected'; ?>>False</option>
+            </select>
+            <br/>
+            <label for="owner">
+              <h6 class="card-subtitle mb-2 text-muted">Owner</h6>
+            </label>
+            <br/>
+            <select class="form-select" name="owner" id="owner">
+              <option value="-1">None</option>
+              <?php
+              for ($i = 0; $i < count($GLOBALS['users']); $i++) {
+                $user = $GLOBALS['users'][$i];
+                $selected = $user->userId == $GLOBALS['server']->ownerId ? 'selected="selected"' : '';
+                echo '<option value="' . $user->userId . '" ' . $selected . '>' . $user->username . '</option>';
+              }
+              ?>
+            </select>
+            <br/>
+            <button type="submit" class="btn btn-primary form-control">Update</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+  function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
+
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.id = 'custom-jar';
+  input.name = 'custom-jar';
+  input.classList.add('form-control');
+  input.value = document.getElementById('serverJar').getAttribute('jar');
+
+  jarChanged(document.getElementById('serverJar'));
+
+  function jarChanged(select) {
+    if (select.value === 'custom') {
+      insertAfter(input, select);
+    } else {
+      if (document.getElementById('custom-jar')) {
+        input.parentNode.removeChild(input);
+      }
+    }
+  }
+</script>
 </body>
 </html>
 
